1d97ef6f8d3d6eb5ceed1f3169f0b3a48e041886
arefome <55114188+arefome@users.noreply.github.com>
2021-12-15 22:02:12 -0500

    Update main.py

--- 15-12-2021/main.py
+++ 15-12-2021/main.py
@@ -1,1 +1,7 @@
 print("Hola :)")
+lista = [10, 20, 30, "hola", True]
+[print(x) for x in lista if type(x) == int]
+tupla = (1, 2, 3, 4)  # Son inmutables
+print(tupla[0])
+[print(i) for i in tupla]
+dict = {}
