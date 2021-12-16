c3f44c2ef9ca314de88a3d1c8fdcc25b7fa6405e
IvanDario141 <idlopezd@correo.udistrital.edu.co>
2021-12-15 21:16:20 -0500

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,22 @@
+#Hola
+mibool = True 
+numeros = 33443 
+string = "uhfjd"
+
+lista = []
+print(lista)
+
+lista=[10,20,30]
+print(lista)
+
+lista = [True,'veinte', 20]
+print(lista)
+
+print(lista[0])
+
+if(lista[1]== 'veinte'):
+    print('Efectivamente es true')
+
+    
+if(lista[0]!= 'veinte'):
+    print('Efectivamente es false')
