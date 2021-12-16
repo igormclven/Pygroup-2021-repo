b56f870b7f427d0b82ce07fcac2dca70bee3936c
Julian Gomez <juliandr544@gmail.com>
2021-12-15 22:01:55 -0500

    :)

---
+++ 15-12/main.py
@@ -0,0 +1,46 @@
+# #160a codeforces
+# n = int(input())
+# dic = []
+# for i in range(n):
+	# dic[i]=int(input())
+	# sum += dic[i]
+# sort(dic)
+# for i in range (n-1 , 0, 1):
+	# sum2+=dic[i]
+	# sum -=dic[i]
+	# counter+=1
+	# if sum2 > sum:
+		# break
+# print(counter)
+# # xd no pude
+
+lista = [50 , 20 , 30]
+print(lista)
+# lista = []
+# tupla no repetidos = ()
+# diccionario = { key-value  }
+if(lista[0]>30):
+	print(f"El num { lista[0] } es mayor a 30")
+else:
+	print(f"El num { lista[0] } no es mayor a 30")
+
+dic_ = { 1: "xd",
+         2: "o:"
+       }
+       # key - value
+print(dic_)
+dic_[1] = "dx"
+print(dic_)
+
+
+###############
+
+n = int(input())
+while n>0:
+	print("hola")
+	n-=1
+
+#######
+
+for key,value in dic_.items():
+	print(key,value)
